ae0ad751f83af1dd9aa10476f7fa854c5113138b
opanevskaa <opanevskaa@gmail.com>
2024-07-24 12:24:55 +0200

    Add files via upload

---
+++ Вебинар 1. Знакомство. Установка python/task1/task.py
@@ -0,0 +1,9 @@
+def hello_world():
+    """Привет, мир!"""
+    hello = 'Hello'
+    world = 'world'
+    hello_and_world = hello + ' ' + world + '!'
+    return hello_and_world
+
+# Пример вызова функции
+print(hello_world())
